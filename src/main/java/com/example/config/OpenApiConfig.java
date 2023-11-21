c376e50c8d19141f02520d70e3c86239f100c3ee
Nedder3 <arij.dev@gmail.com>
2023-11-20 21:34:33 -0300

    add refactor

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -8,23 +8,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /*
-    http://localhost:8000/v3/api-docs
-    http://localhost:8000/swagger-ui/index.html
-*/
-
+http://localhost:8080/v3/api-docs
+http://localhost:8080/swagger-ui/index.html
+ */
 @Configuration
 public class OpenApiConfig {
-    //se marca con bean para que el framework lea el objeto
+
     @Bean
-    public OpenAPI openAPI(){
-        return new OpenAPI().info(new Info()
+    public OpenAPI openApi(){
+        return new OpenAPI()
+                .info(new Info()
                         .title("Employees API")
-                        .description("API REST para empleados de la empresa x")
-                        .version("v0.0.01")
+                        .description("API REST para empleados de la empresa X")
+                        .version("v0.0.1")
                         .license(new License().name("Apache 2.0").url("https://www.example.com/")))
                 .externalDocs(new ExternalDocumentation()
                         .description("Wiki Docs")
                         .url("https://www.example.com/"));
-
     }
+
 }
--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -9,22 +9,31 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @Tag(name = "Employee", description = "Employee API REST with CRUD Operations")
 public class EmployeeController {
+
     @ApiResponses(value = {
-            @ApiResponse(responseCode ="200", description = "Retrieved employees",content = {
+            @ApiResponse(responseCode = "200", description = "Retrieved employees", content = {
                     @Content(mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation = Employee.class)))})
+                            array = @ArraySchema(schema = @Schema(implementation = Employee.class)))})
     })
-    @Operation(summary = "findAll Employess",description = "it retrieves all employees from database without pagination")  //documentamos el metodo
-    @RequestMapping("/employee")
-    public ResponseEntity<List<Employee>> findAll(){
-        return null;
+    @Operation(summary = "findAll employees", description = "It retrieves all employees from database without pagination")
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> findAll() {
+        return ResponseEntity.ok(List.of(
+                new Employee(1L, "emp1", LocalDate.of(1990, 1, 1), 40000.0, true),
+                new Employee(2L, "emp2", LocalDate.of(1990, 1, 1), 40000.0, true),
+                new Employee(3L, "emp3", LocalDate.of(1990, 1, 1), 40000.0, true)
+        ));
     }
+
 }
